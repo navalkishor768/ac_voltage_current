9034c38e2724097c7f86c24a2a45b3da929736bc
Navalkishor <navalkishor768@gmail.com>
2022-08-29 20:32:05 +0530

    intial files added

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * ADC  Timer Interrupt.c
+ *
+ * Created: 29-Aug-22 8:30:55 PM
+ * Author : admin
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
