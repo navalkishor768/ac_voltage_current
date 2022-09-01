0ba5410be4328dfd7480d2c6765dec3ea8837094
Navalkishor <navalkishor768@gmail.com>
2022-09-02 00:15:01 +0530

    LCD Lib added, Timer 0 removed, using Timer 2 for reading and task scheduling

--- main.c
+++ main.c
@@ -13,37 +13,33 @@
 #include "adcatmega16.C"
 #include "uartnaval.h"
 #include "uartnaval.c"
+#include "lcdgdheerajat8.h"
+#include "lcdgdheerajat8.c"
 
-#define T0_CNT_VAL			24
-#define	T1_PRESCALAR		64
-#define HZ_SAMPLE_CNT		5.0
+#define T0_CNT_VAL			30
+#define	T1_PRESCALAR		1024
+#define HZ_SAMPLE_CNT		10
 #define	BASE				(F_CPU/(T1_PRESCALAR*2))
 #define SAMPLE_CNT			100
-#define PEAK_VAC			600
-#define ADC_PVAC_FSCALE		4.25
-#define VAC_MUL_FAC			(PEAK_VAC/ADC_PVAC_FSCALE)
+#define PEAK_VAC			311
+#define ADC_PVAC_FSCALE		2.205
+#define SCALE_MUL_FAC		(PEAK_VAC/ADC_PVAC_FSCALE)
 #define ADC_REAL_MUL		(5/1024.0)
 //#define MV_PER_AMP		0.2			// for ACS712 5A module
 #define MV_PER_AMP			0.1			// for ACS712 20A module
 //#define MV_PER_AMP		0.066		// for ACS712 30A module
+#define error_per			1.1
 
-unsigned int adcVac,adcVacMax,adcCurr,adcCurrMax,i,j;
+unsigned int adcVac,adcVacMax,adcCurr,i,j,VacSampleCNT = 0,CurrSampleCNT=0,sampleCNT=0;
+unsigned long int adcVacSq=0,adcCurrMax=0;
 float RmsVoltage,RmsCurrent,freq;
 unsigned char str[50],str1[10];
 
-volatile int flag_10ms=0;
 volatile int flag_1000ms=0;
 volatile int flag_500ms=0;
 
 
 float getfreq(void);
-
-void init_t0(int t0_cnt_val)
-{
-	TCCR0 = 0X05;			//	PRESCALAR AS 1024
-	TCNT0 = t0_cnt_val;		
-	TIMSK |= (1 << TOIE0);  //	Unmask Timer 0 overflow interrupt.
-}
 
 void init_t2()
 {
@@ -56,56 +52,52 @@ void init_t1_input_capture(void)	 /* function to initialize Timer1 input capture
 {
 	TCCR1A = 0;
 	TIFR = (1<<ICF1);				/* clear input capture flag */
-	TCCR1B = 0x03;					/* capture on falling edge, prescalar 64 */
-}
-
-ISR(TIMER0_OVF_vect)
-{	
-	adcVacMax = 0;
-	adcCurrMax = 0;
-	
-	for(i=0;i<SAMPLE_CNT;i++)
-	{
-		adcVac = ReadADC(0);
-		
-		if(adcVac >= adcVacMax)
-				adcVacMax = adcVac;
-		
-		adcCurr = ReadADC(1);
-		
-		if((adcCurr >= 512) && (adcCurr >= adcCurrMax))
-										adcCurrMax = adcCurr;
-	}
-	TCNT0 = T0_CNT_VAL;
+	TCCR1B = 0x05;					/* capture on falling edge, prescalar 1024 */
 }
 
 ISR(TIMER2_OVF_vect)
 {
 	static	int count_1000ms=0;
-	static	int count_10ms=0;
 	static	int count_500ms=0;
 	
-	if(count_1000ms == 1000)
+	static long int adcVacSqSmpl=0,locSampleCNT=0,adcCurrSmpl=0,locCurrSmplCNT=0;
+	adcVac = ReadADC(0);
+	
+	if(adcVac >= 200)	
 	{
+		adcVacSqSmpl += adcVac * ADC_REAL_MUL * adcVac * ADC_REAL_MUL;
+		locSampleCNT++;
+	}
+	
+	adcCurr = ReadADC(1);
+	if(adcCurr >= adcCurrSmpl)
+	{
+		adcCurrSmpl = adcCurr;
+		locCurrSmplCNT++;
+	}
+	
+	if(count_1000ms == 1000)
+	{	
+		adcCurrMax = adcCurrSmpl;
+		adcVacSq = adcVacSqSmpl;
+		adcVacSqSmpl = 0;
+		adcCurrSmpl = 0;
+		sampleCNT = locSampleCNT;
+		CurrSampleCNT = locCurrSmplCNT;
+		locSampleCNT=0;
+		locCurrSmplCNT = 0;
 		flag_1000ms = 1;
 		count_1000ms = 0;
 	}
 	
-	if(count_10ms == 10)
-	{
-		flag_10ms = 1;
-		count_10ms = 0;
-	}
-	
 	if(count_500ms == 500)
-	{
+	{	
 		flag_500ms = 1;
 		count_500ms = 0;
 	}
 	
 	count_500ms++;
 	count_1000ms++;
-	count_10ms++;
 	TCNT2 = 240;
 }
 
@@ -116,7 +108,8 @@ int main(void)
 	
 	uart_init(9600);
 	
-	init_t0(T0_CNT_VAL);
+	lcd_init(LCD_DISP_ON);
+	_delay_ms(100);
 	
 	init_t2();
 	
@@ -129,36 +122,53 @@ int main(void)
     while (1) 
     
 		
-		if(flag_10ms == 1)
-		{
-			RmsVoltage = adcVacMax * 0.707 * ADC_REAL_MUL * VAC_MUL_FAC;
+		if(flag_1000ms == 1)
+		{	
+			RmsVoltage = SCALE_MUL_FAC * sqrt(adcVacSq / sampleCNT) * error_per;
+			
 			RmsCurrent = (adcCurrMax - 512) * 0.707 * ADC_REAL_MUL / MV_PER_AMP;
 			
-			flag_10ms = 0;
+			/*
+			Convert Float Values to string to transmit on UART
+			*/
 			
-		}
-		
-		else if(flag_1000ms == 1)
-		{
-			dtostrf(getfreq(),2,1,str1);
-			sprintf(str,"\rFrequency is %s Hz",str1);
+			dtostrf(RmsVoltage,2,1,str1);
+			sprintf(str,"\rRMS Voltage = %s",str1);
+			uart_txstr(str);
+			
+			dtostrf(RmsCurrent,2,1,str1);
+			sprintf(str," RMS Current = %s A",str1);
+			uart_txstr(str);
+			
+			freq = getfreq();
+			dtostrf(freq,2,1,str1);
+			sprintf(str,"\rFrequency = %s Hz",str1);
 			uart_txstr(str);
 			uart_tx('\r');
-			TIMSK = 0x41;
+			TIMSK = 0x41;		// enable timer interrupts
+			
 			flag_1000ms = 0;
 		}
 		
 		else if(flag_500ms == 1)
 		{
+			/* 
+			Display RMS Voltage, Current and Freq on 16x2 LCD
+			*/
+			lcd_clrscr();
 			dtostrf(RmsVoltage,2,1,str1);
-			sprintf(str,"\rRMS Voltage is %s",str1);
-			uart_txstr(str);
+			lcd_putsxy(0,0,"VRMS=");
+			lcd_putsxy(5,0,str1);
 			
 			dtostrf(RmsCurrent,2,1,str1);
-			sprintf(str," RMS Current is %s A",str1);
-			uart_txstr(str);
+			lcd_putsxy(5,1,"000");
+			lcd_putsxy(0,1,"IRMS=");
+			lcd_putsxy(5,1,str1);
+			lcd_putsxy(9,1,"A");
 			
-			uart_tx('\r');
+			dtostrf(freq,2,1,str1);
+			lcd_putsxy(11,1," F=");
+			lcd_putsxy(14,1,str1);
 			
 			flag_500ms = 0;
 		}
@@ -167,7 +177,7 @@ int main(void)
 
 float getfreq(void)
 {
-	TIMSK = 0x00;
+	TIMSK = 0x00;		// disable timer interrupts for input capture reading
 	
 	unsigned int timePEdge=0, timeEdge1;
 	
